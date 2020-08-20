cf9881f6a78fa84c0341d42a728f8f3455b4b124
KamilLatipov <latipovkamil@mail.ru>
2020-08-20 14:19:27 +0300

    new blocks

--- src/page/main.js
+++ src/page/main.js
@@ -1,6 +1,7 @@
 import "../blocks/gis-win/gis-win.scss";
 import "../scss/global.scss";
 import "../scss/slick.scss";
+import "../scss/ion.rangeSlider.scss";
 import "../blocks/header/header.scss";
 import "../blocks/first-block/first-block.scss";
 import "./landing.scss";
@@ -24,63 +25,176 @@ import "../blocks/nineth-block/nineth-block.scss";
 import "../blocks/tenth-block/tenth-block.scss";
 import "../blocks/footer/footer.scss";
 import "../js/slick.min";
-
-
-function calculate(rangeId, inputId, check) {
-  let calculator = document.getElementById(rangeId);
-  let month = document.getElementsByClassName('month');
-  let monthtext = document.getElementsByClassName('month-text');
-  let year = document.getElementById('income-year');
-  let gain = document.getElementsByClassName('gain');
-
-  calculator.addEventListener('input', () => {
-    let input = document.getElementById(inputId);
-    let value = calculator.value * check * 30 + "";
-    value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
-    input.value = value + " ₽";
-
-    let therapy = parseInt(document.getElementById('input-therapy').value.replace(/\s+/g, '').replace("₽", ""));
-    let prosthesis =  parseInt(document.getElementById('input-prosthesis').value.replace(/\s+/g, '').replace("₽", ""));
-    let surgery =  parseInt(document.getElementById('input-surgery').value.replace(/\s+/g, '').replace("₽", ""));
-    let implant =  parseInt(document.getElementById('input-implant').value.replace(/\s+/g, '').replace("₽", ""));
-
-    let sum = 1*(therapy + prosthesis + surgery + implant) + "";
-    let totalGain = 1*(therapy + prosthesis + surgery + implant) - 1740500 + "";
-    let yearSum = totalGain * 12 + "";
-    totalGain = totalGain.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
-    yearSum = yearSum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
-
-    gain[0].innerHTML = totalGain + " ₽";
-    year.innerHTML = yearSum + " ₽";
-
-    sum = sum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
-
-    month[0].innerHTML = sum + " ₽";
-    monthtext[0].innerHTML = totalGain + " ₽ в месяц";
-
-    let val = calculator.value - calculator.attr('min') / calculator.attr('max') - calculator.attr('min');
-
-    calculator.css('background-image',
-      '-webkit-gradient(linear, left top, right top, '
-      + 'color-stop(' + val + ', #94A14E), '
-      + 'color-stop(' + val + ', #C5C5C5)'
-      + ')'
-    );
-  })
-}
-
-calculate('range-therapy', 'input-therapy', 3500);
-calculate('range-prosthesis', 'input-prosthesis', 5000);
-calculate('range-surgery', 'input-surgery', 3000);
-calculate('range-implant', 'input-implant', 30000);
+import "../js/ion.rangeSlider.min";
 
 $(document).ready(function(){
   $('.fifth-block__gallery').slick({
-    dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true
   });
+  $(".slick-prev").append('<i class="fa fa-angle-left" aria-hidden="true"></i>');
+  $(".slick-next").append('<i class="fa fa-angle-right" aria-hidden="true"></i>');
+});
+
+$("#range-therapy").ionRangeSlider({
+  min: 5,
+  max: 15,
+  from: 10,
+  step: 1
+});
+
+$("#range-prosthesis").ionRangeSlider({
+  min: 5,
+  max: 15,
+  from: 10,
+  step: 1
+});
+
+$("#range-surgery").ionRangeSlider({
+  min: 5,
+  max: 15,
+  from: 10,
+  step: 1
+});
+
+$("#range-implant").ionRangeSlider({
+  min: 5,
+  max: 15,
+  from: 10,
+  step: 1
+});
+
+$("#range-therapy").on("change", function() {
+  let $inp = $(this);
+
+  let month = document.getElementsByClassName('month');
+  let monthtext = document.getElementsByClassName('month-text');
+  let year = document.getElementById('income-year');
+  let gain = document.getElementsByClassName('gain');
+
+  let input = document.getElementById('input-therapy');
+  let value = $inp.prop("value") * 3500 * 30 + "";
+  value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  input.value = value + " ₽";
+
+  let therapy = parseInt(document.getElementById('input-therapy').value.replace(/\s+/g, '').replace("₽", ""));
+  let prosthesis =  parseInt(document.getElementById('input-prosthesis').value.replace(/\s+/g, '').replace("₽", ""));
+  let surgery =  parseInt(document.getElementById('input-surgery').value.replace(/\s+/g, '').replace("₽", ""));
+  let implant =  parseInt(document.getElementById('input-implant').value.replace(/\s+/g, '').replace("₽", ""));
+
+  let sum = 1*(therapy + prosthesis + surgery + implant) + "";
+  let totalGain = 1*(therapy + prosthesis + surgery + implant) - 1740500 + "";
+  let yearSum = totalGain * 12 + "";
+  totalGain = totalGain.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  yearSum = yearSum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  gain[0].innerHTML = totalGain + " ₽";
+  year.innerHTML = yearSum + " ₽";
+
+  sum = sum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  month[0].innerHTML = sum + " ₽";
+  monthtext[0].innerHTML = totalGain + " ₽ в месяц";
+});
+
+$("#range-prosthesis").on("change", function() {
+  let $inp = $(this);
+
+  let month = document.getElementsByClassName('month');
+  let monthtext = document.getElementsByClassName('month-text');
+  let year = document.getElementById('income-year');
+  let gain = document.getElementsByClassName('gain');
+
+  let input = document.getElementById('input-prosthesis');
+  let value = $inp.prop("value") * 5000 * 30 + "";
+  value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  input.value = value + " ₽";
+
+  let therapy = parseInt(document.getElementById('input-therapy').value.replace(/\s+/g, '').replace("₽", ""));
+  let prosthesis =  parseInt(document.getElementById('input-prosthesis').value.replace(/\s+/g, '').replace("₽", ""));
+  let surgery =  parseInt(document.getElementById('input-surgery').value.replace(/\s+/g, '').replace("₽", ""));
+  let implant =  parseInt(document.getElementById('input-implant').value.replace(/\s+/g, '').replace("₽", ""));
+
+  let sum = 1*(therapy + prosthesis + surgery + implant) + "";
+  let totalGain = 1*(therapy + prosthesis + surgery + implant) - 1740500 + "";
+  let yearSum = totalGain * 12 + "";
+  totalGain = totalGain.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  yearSum = yearSum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  gain[0].innerHTML = totalGain + " ₽";
+  year.innerHTML = yearSum + " ₽";
+
+  sum = sum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  month[0].innerHTML = sum + " ₽";
+  monthtext[0].innerHTML = totalGain + " ₽ в месяц";
+});
+
+$("#range-surgery").on("change", function() {
+  let $inp = $(this);
+
+  let month = document.getElementsByClassName('month');
+  let monthtext = document.getElementsByClassName('month-text');
+  let year = document.getElementById('income-year');
+  let gain = document.getElementsByClassName('gain');
+
+  let input = document.getElementById('input-surgery');
+  let value = $inp.prop("value") * 3000 * 30 + "";
+  value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  input.value = value + " ₽";
+
+  let therapy = parseInt(document.getElementById('input-therapy').value.replace(/\s+/g, '').replace("₽", ""));
+  let prosthesis =  parseInt(document.getElementById('input-prosthesis').value.replace(/\s+/g, '').replace("₽", ""));
+  let surgery =  parseInt(document.getElementById('input-surgery').value.replace(/\s+/g, '').replace("₽", ""));
+  let implant =  parseInt(document.getElementById('input-implant').value.replace(/\s+/g, '').replace("₽", ""));
+
+  let sum = 1*(therapy + prosthesis + surgery + implant) + "";
+  let totalGain = 1*(therapy + prosthesis + surgery + implant) - 1740500 + "";
+  let yearSum = totalGain * 12 + "";
+  totalGain = totalGain.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  yearSum = yearSum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  gain[0].innerHTML = totalGain + " ₽";
+  year.innerHTML = yearSum + " ₽";
+
+  sum = sum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  month[0].innerHTML = sum + " ₽";
+  monthtext[0].innerHTML = totalGain + " ₽ в месяц";
+});
+
+$("#range-implant").on("change", function() {
+  let $inp = $(this);
+
+  let month = document.getElementsByClassName('month');
+  let monthtext = document.getElementsByClassName('month-text');
+  let year = document.getElementById('income-year');
+  let gain = document.getElementsByClassName('gain');
+
+  let input = document.getElementById('input-implant');
+  let value = $inp.prop("value") * 30000 * 30 + "";
+  value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  input.value = value + " ₽";
+
+  let therapy = parseInt(document.getElementById('input-therapy').value.replace(/\s+/g, '').replace("₽", ""));
+  let prosthesis =  parseInt(document.getElementById('input-prosthesis').value.replace(/\s+/g, '').replace("₽", ""));
+  let surgery =  parseInt(document.getElementById('input-surgery').value.replace(/\s+/g, '').replace("₽", ""));
+  let implant =  parseInt(document.getElementById('input-implant').value.replace(/\s+/g, '').replace("₽", ""));
+
+  let sum = 1*(therapy + prosthesis + surgery + implant) + "";
+  let totalGain = 1*(therapy + prosthesis + surgery + implant) - 1740500 + "";
+  let yearSum = totalGain * 12 + "";
+  totalGain = totalGain.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+  yearSum = yearSum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  gain[0].innerHTML = totalGain + " ₽";
+  year.innerHTML = yearSum + " ₽";
+
+  sum = sum.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+  month[0].innerHTML = sum + " ₽";
+  monthtext[0].innerHTML = totalGain + " ₽ в месяц";
 });
